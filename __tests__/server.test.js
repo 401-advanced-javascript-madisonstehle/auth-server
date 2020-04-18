1c62d86b045f35c704b92144ad16406f99280e91
Madison Stehle <mnstehle3@gmail.com>
2020-04-18 11:26:11 -0700

    wrote endpoint tests

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -5,9 +5,12 @@ const supergoose = require('@code-fellows/supergoose');
 
 const mockRequest = supergoose(app.server);
 
-describe('happy path', () => {
+describe('endpoints', () => {
   it('to save a new user', async () => {
-    let response = await mockRequest.post('/signup').send({username: 'mUser', password: 'mPass'});
+    let response = await mockRequest.post('/signup').set('Authorization', 'Basic bVVzZXI6bVBhc3M=').send({
+      username: 'mUser', 
+      password: 'mPass'
+    });
 
     expect(response.status).toBe(201);
     expect(response.body._id).toBeDefined();
@@ -16,15 +19,16 @@ describe('happy path', () => {
   });
 
   it('to retrieve an existing user', async () => {
-    let response = await mockRequest.post('/signin').set('Authorization', 'Basic ');
+    let response = await mockRequest.post('/signin').set('Authorization', 'Basic bVVzZXI6bVBhc3M=');
 
     expect(response.status).toBe(200);
     expect(response.body.username).toBe('mUser');
   });
-});
 
-describe('expected failure', () => {
-  it('to throw an error if the user doesn\'t exist', () => {
+  it('to retrieve all users', async () => {
+    let response = await mockRequest.get('/users');
 
-  })
+    expect(response.status).toBe(200);
+    expect(response.body).toBeDefined();
+  });
 });
